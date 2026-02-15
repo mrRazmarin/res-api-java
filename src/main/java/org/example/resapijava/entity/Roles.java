ea85dbee1ca6959af804e37f29a7eaede4ea18a1
Mikhail Salnikov <salnikovmishanew@gmail.com>
2026-02-15 12:38:08 +0300

    Добавил таблицу ролей и связал ее с пользователями

---
+++ src/main/java/org/example/resapijava/entity/Roles.java
@@ -0,0 +1,27 @@
+package org.example.resapijava.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "roles")
+public class Roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @ToString.Exclude
+    private Set<User> users = new HashSet<>();
+}
